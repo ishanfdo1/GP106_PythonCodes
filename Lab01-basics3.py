af9f42fc4929e7c288b37bf340119f31d37224aa
ishanfdo1 <70000401+ishanfdo1@users.noreply.github.com>
2020-08-21 09:53:00 +0530

    Add files via upload

---
+++ Lab01-basics3.py
@@ -0,0 +1,15 @@
+x = 1
+y = 2
+z = 3
+
+print(- 10 < x and x < 10)  # Variable x lies between -10.0 and 10.0
+
+print(x + y == z)  # The sum of variables x and y is equal to the value of variable z
+
+print(x < -10 or x > 10)  # Variable x that lies outside the range -10.0 and 10.0
+
+print(x > 0 and x < 10)  # Variable x is a positive number which is smaller than 10.0
+
+
+
+
